17d8392170710d2e32319415187d5acd41028e5c
Michael Lin <c65lin@gmail.com>
2025-11-26 22:12:27 -0500

    learn + edit the folder 11/26

---
+++ 8variable-numbersystems/exercisebygpt.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int number1= 42;
+    int number2= 052;
+    int number3 = 0x2a;
+    int number4 = 0b101010;
+
+    std::cout << number1 << std::endl;
+    std::cout << number2 << std::endl;
+    std::cout << number3 << std::endl;
+    std::cout << number4 << std::endl;
+    return 0;
+}
